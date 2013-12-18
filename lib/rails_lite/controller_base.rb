dc45428778d471f4423af91e2d1274c2657d6142
enquea <enquea@gmail.com>
2013-12-18 11:02:20 -0800

    basic controller base

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -7,20 +7,33 @@ class ControllerBase
 
   # setup the controller
   def initialize(req, res, route_params = {})
+    @req, @res = req, res
+    @already_rendered = false
   end
 
   # populate the response with content
   # set the responses content type to the given type
   # later raise an error if the developer tries to double render
   def render_content(content, type)
+    self.check_and_set_built_response_flag
+    @res.body = content
+    @res.content_type = type
+  end
+
+  def check_and_set_built_response_flag
+    raise "Already built response" if already_rendered?
+    @already_rendered = true
   end
 
   # helper method to alias @already_rendered
   def already_rendered?
+    @already_rendered
   end
 
   # set the response status code and header
   def redirect_to(url)
+    self.check_and_set_built_response_flag
+    @res.set_redirect(WEBrick::HTTPStatus::TemporaryRedirect, url)
   end
 
   # use ERB and binding to evaluate templates
---
+++ test/my_first_server.rb
@@ -0,0 +1,15 @@
+require 'webrick'
+
+server = WEBrick::HTTPServer.new(:Port => 3000)
+
+server.mount_proc "/" do |req, res|
+  res.content_type = "text/text"
+  res.body = req.path
+end
+
+
+
+
+
+trap('INT') { server.shutdown }
+server.start
